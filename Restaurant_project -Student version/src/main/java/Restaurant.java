03675cd75210c1b0b9091ad1922bed8854b8c27c
A-Jeet <abhayjeetwork@gmail.com>
2020-12-07 14:41:13 +0530

    Part 2: Solution

---
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+
+        if((getCurrentTime().isAfter(openingTime)) && getCurrentTime().isBefore(closingTime)){  //checks if the user entry time lies within the operating time of the selected Restaurant.
+            return true;
+        }
+        return false;
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return this.menu;
+    }
+
+    private Item findItemByName(String itemName){ //searches item in menu object.
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName + "restaurant is not present in the list");
+
+        menu.remove(itemToBeRemoved);
+    }
+
+
+    public void displayDetails(){ System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
